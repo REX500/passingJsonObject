089f9db3b0a5f848e513c10a1c941fbae58ca4d4
Filip <filamalek@hotmail.com>
2017-02-19 19:48:06 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ var one = require(__dirname+"/one.js");
 // method that initializes the flow of passing the object
 
 one.fnStartPassingTheObject();
+
+app.listen(10008 , function(){
+  console.log("Server listening to port 10008");
+});
